3379ea117ab7463346b6ff52cd6df7676b587586
Graziella Custódio <graziella_mc@hotmail.com>
2026-02-04 20:46:51 +0000

    homepage select working

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const nav = document.getElementById("nav");
+const selectPosts = document.getElementById("selectPosts");
+const latest_column = document.getElementById("latest_column");
+const project_column = document.getElementById("project_column");
+const mq_mobile = window.matchMedia("(max-width: 576px)");
+
+function mudar_coluna (valor) {
+    if (!mq_mobile.matches) return;
+    latest_column.classList.toggle("hidden", valor === "other_projects");
+    project_column.classList.toggle("hidden", valor === "latest_entries")
+}
+
+if (selectPosts) {
+    mudar_coluna(selectPosts.value);
+    selectPosts.addEventListener("change", function () {
+        mudar_coluna(this.value);
+    })
+}
 function openNav() {
     nav.classList.add("show");
 }
